df73b7e56783d7aa821946016770d17db4394861
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-01-22 09:33:43 +0100

    JAVA - 217. Contains Duplicate

---
+++ Algorithms/217. Contains Duplicate/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int val : nums) {
+            if(set.contains(val)) { 
+                return true;
+            }
+            set.add(val);
+        }
+        return false;
+    }
+}
