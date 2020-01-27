1426af3f2e795958f27931cc58cb75d7c7a4c0ed
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-01-27 11:14:23 +0100

    JAVA - 258. Add Digits

---
+++ Algorithms/258. Add Digits/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+        
+        if (num < 10) {
+            return num;
+        }
+        
+        int res = num % 9;
+        
+        if ( res == 0) {
+            return 9;
+        }
+        
+        return res;
+    }
+}
