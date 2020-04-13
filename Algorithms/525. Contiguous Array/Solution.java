5da088dd90d15ab05546a1704bc57eefb51f23c6
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2020-04-13 22:44:07 +0200

    JAVA - 525. Contiguous Array

---
+++ Algorithms/525. Contiguous Array/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int sum = 0;
+        int res = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                nums[i] = -1;
+            }
+            sum += nums[i];
+            if(map.containsKey(sum)) {
+                res = Math.max(res, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        
+        return res;
+    }
+}
