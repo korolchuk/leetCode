e39dd27c0b4c316451927f06ef63a0132ad8c63d
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2020-08-27 23:59:42 +0200

    Binary Tree - Binary Tree Preorder Traversal - iterative

---
+++ Binary Tree/Binary Tree Preorder Traversal/Solution+.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while(!stack.empty()) {
+            TreeNode tmp = stack.pop();
+            res.add(tmp.val);
+            if(tmp.right != null) {
+                stack.push(tmp.right);
+            }
+            if(tmp.left != null) {
+                stack.push(tmp.left);
+            }
+        }
+        return res;
+    }
+    
+}
