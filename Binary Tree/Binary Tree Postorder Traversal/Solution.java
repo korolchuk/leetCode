d31dc28ddf390444edf2b915bafcef42e9106cdb
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-08-28 13:56:49 +0200

    Binary Tree - Binary Tree Postorder Traversal - recursive

---
+++ Binary Tree/Binary Tree Postorder Traversal/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> res = new LinkedList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        helper(root);
+        return res;
+    }
+    
+    public void helper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helper(root.left);
+        helper(root.right);
+        
+        res.add(root.val);
+        return;
+    }
+    
+}
