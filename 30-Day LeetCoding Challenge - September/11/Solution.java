39c1379c910571b4b1998b1b3d3f8274ab46b36c
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2020-09-11 22:37:07 +0200

    30-Day LeetCoding Challenge - September - Day 11

---
+++ 30-Day LeetCoding Challenge - September/11/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            int tmp = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(tmp * nums[i], min * nums[i]), nums[i]);
+            res = Math.max(max, res);
+        }
+        return res;
+    }
+}
