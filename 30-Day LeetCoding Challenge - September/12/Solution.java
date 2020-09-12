7374cef52d21fe2a559d44147490f3314869e2b5
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2020-09-12 23:19:46 +0200

    30-Day LeetCoding Challenge - September - Day 12

---
+++ 30-Day LeetCoding Challenge - September/12/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> comp = new ArrayList<Integer>();
+        helper(res, comp, k, 1, n);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> comb, int k,  int start, int n) {
+        if (comb.size() == k && n == 0) {
+            List<Integer> l = new ArrayList<Integer>(comb);
+            res.add(l);
+            return;
+        }
+        for (int i = start; i <= 9; i++) {
+            comb.add(i);
+            helper(res, comb, k, i+1, n-i);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
