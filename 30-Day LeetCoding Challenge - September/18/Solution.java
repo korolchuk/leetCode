4977e94f91f2b2720893aadf12642821ee4e5715
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-09-18 11:18:48 +0200

    30-Day LeetCoding Challenge - September - Day 18

---
+++ 30-Day LeetCoding Challenge - September/18/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length < 2) {
+            return 0;
+        }
+        int min = prices[0];
+        int max = 0;
+        for(int i = 1; i < prices.length; i++) {
+            if (prices[i] > min) {
+                max = Math.max(max, prices[i] - min);
+            }
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+}
