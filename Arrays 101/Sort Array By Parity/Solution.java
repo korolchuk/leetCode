8e00279b0c4244bc6d053c7a18364ac8f1df912f
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-10-01 14:53:19 +0200

    Arrays 101 - Sort Array By Parity

---
+++ Arrays 101/Sort Array By Parity/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int start = 0;
+        int end = A.length - 1;
+        while(start < end) {
+            if(A[start] % 2 == 0) {
+                start++;
+            } else if (A[end] % 2 == 1) {
+                        end--;
+            } else {
+                int tmp = A[end];
+                A[end] = A[start];
+                A[start] = tmp;
+            }
+        }
+        
+        return A;
+    }
+}
