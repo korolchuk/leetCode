bf2b8c76e8ac73fd776df1813f59a375a7595701
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-10-14 10:35:34 +0200

    Array and String - Plus One

---
+++ Array and String/Plus One/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int plus = 1;
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] + plus == 10) {
+                digits[i] = 0;
+                plus = 1;
+             } else {
+                digits[i] = digits[i] + plus;
+                plus = Math.max(0, plus - 1);
+            } 
+        }
+        if(plus == 1) {
+            int[] res = new int[digits.length + 1];
+            res[0] = 1;
+            return res;
+        }
+        return digits;
+    }
+}
