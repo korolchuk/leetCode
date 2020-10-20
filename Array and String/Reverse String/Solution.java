6a11bc448bb8d5212ac22a1bd2a6b3079ef16f81
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-10-20 10:47:46 +0200

    Array and String - Reverse String

---
+++ Array and String/Reverse String/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while(i < j) {
+            if(s[i] != s[j]) {
+                char tmp = s[j];
+                s[j] = s[i];
+                s[i] = tmp;
+            }
+            i++;
+            j--;
+        }
+    }
+}
