603f42970ffd59617e0d2b26ed36e14ff0622639
Uladzimir KARALCHUK <u.karalchuk@FIDUCIAL.DOM>
2020-10-28 16:05:38 +0100

    Hash Table - Single Number

---
+++ Hash Table/Single Number/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums) {
+            if(set.contains(n)) {
+                set.remove(n);
+            } else {
+                set.add(n);
+            }
+        }
+        return set.iterator().next();
+    }
+}
